c3d91cb7a45042830fbe38ca02a7502b7d6f2e8b
haurui <huarui@lingxing.com>
2025-01-22 17:07:13 +0800

    feat: 设置数据库默认字符串长度并强制HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 设置数据库默认字符串长度为255
+        Schema::defaultStringLength(255);
+
+        // 检查环境变量是否设置了强制使用 HTTPS
+        if (env('APP_FORCE_HTTPS', false)) {
+            // 强制使用 HTTPS
+            URL::forceScheme('https');
+        }
     }
 }
