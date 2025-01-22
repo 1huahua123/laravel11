6680bd9576c68e28a1ef2f8fed7aa5c242ffd0c0
haurui <huarui@lingxing.com>
2025-01-22 17:58:12 +0800

    feat: 添加SettingModel和BaseModel基础模型

---
+++ innopacks/common/src/Models/BaseModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace InnoShop\Common\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class BaseModel
+ * @package InnoShop\Common\Models
+ * @author 华锐
+ * @desc 基础模型
+ */
+class BaseModel extends Model
+{
+
+}
