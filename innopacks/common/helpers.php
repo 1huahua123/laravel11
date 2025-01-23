f1b9edfc26ac58f4522c5712cf21b4641bacb5b0
haurui <huarui@lingxing.com>
2025-01-23 14:19:01 +0800

    feat: 添加加载设置和检查安装功能

--- innopacks/common/helpers.php
+++ innopacks/common/helpers.php
@@ -1,6 +1,52 @@
 <?php
 
 use Barryvdh\Debugbar\Facades\Debugbar;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+use InnoShop\Common\Repositories\SettingRepo;
+
+if (!function_exists('load_settings')) {
+    /**
+     * 加载设置
+     *
+     * @return void
+     */
+    function load_settings(): void
+    {
+        if (!installed()) {
+            return;
+        }
+
+        if (config('inno')) {
+            return;
+        }
+
+        $result = SettingRepo::getInstance()->groupedSettings();
+        config(['inno' => $result]);
+    }
+}
+
+if (!function_exists('installed')) {
+    /**
+     * 检查是否已安装
+     *
+     * @return bool
+     */
+    function installed(): bool
+    {
+        try {
+            if (Schema::hasTable('settings') && file_exists(storage_path('installed'))) {
+                return true;
+            }
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+
+            return false;
+        }
+
+        return false;
+    }
+}
 
 if (!function_exists('has_debugbar')) {
     /**
