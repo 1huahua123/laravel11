4bf3a8a1c208081dc16c5a913b3fee606ef1a7f8
haurui <huarui@lingxing.com>
2025-01-23 14:14:23 +0800

    feat: 增加设置管理功能及异常处理

--- innopacks/common/src/Repositories/SettingRepo.php
+++ innopacks/common/src/Repositories/SettingRepo.php
@@ -4,6 +4,7 @@ namespace InnoShop\Common\Repositories;
 
 use Illuminate\Database\Eloquent\Builder;
 use InnoShop\Common\Models\SettingModel;
+use Throwable;
 
 /**
  * Class SettingRepo
@@ -14,7 +15,7 @@ use InnoShop\Common\Models\SettingModel;
 class SettingRepo extends BaseRepo
 {
     /**
-     *  获取设置查询构造器
+     * 获取设置查询构造器
      *
      * @param array $filters
      * @return Builder
@@ -22,7 +23,7 @@ class SettingRepo extends BaseRepo
     public function builder(array $filters = []): Builder
     {
         $builder = SettingModel::query();
-        $space = $filters['space'] ?? '';
+        $space   = $filters['space'] ?? '';
         if ($space) {
             $builder->where('space', $space);
         }
@@ -33,5 +34,108 @@ class SettingRepo extends BaseRepo
         }
 
         return fire_hook_filter('repo.setting.builder', $builder);
+    }
+
+    /**
+     * 获取设置的所有记录
+     *
+     * @return array
+     */
+    public function groupedSettings(): array
+    {
+        $settings = SettingModel::all(['space', 'name', 'value', 'json']);
+
+        $result = [];
+        foreach ($settings as $setting) {
+            $space = $setting->space;
+            $name  = $setting->name;
+            $value = $setting->value;
+            if ($setting->json) {
+                $result[$space][$name] = json_decode($value, true);
+            } else {
+                $result[$space][$name] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * 批量更新设置值
+     *
+     * @param $settings - 设置数组 ['name' => 'value']
+     * @param string $space - 设置所属空间
+     * @return void
+     * @throws Throwable
+     */
+    public function updateValues($settings, string $space = 'system'): void
+    {
+        foreach ($settings as $name => $value) {
+            if (in_array($name, ['_method', '_token'])) {
+                continue;
+            }
+            $this->updateValue($name, $value, $space);
+        }
+    }
+
+    /**
+     * 更新系统设置值
+     *
+     * @param $name - 设置名称
+     * @param $value - 设置值
+     * @return SettingModel|null
+     * @throws Throwable
+     */
+    public function updateSystemValue($name, $value): ?SettingModel
+    {
+        return $this->updateValue($name, $value, 'system');
+    }
+
+    /**
+     * 更新插件的设置值
+     *
+     * @param $code - 插件代码
+     * @param $name - 设置名称
+     * @param $value - 设置值
+     * @return SettingModel|null
+     * @throws Throwable
+     */
+    public function updatePluginValue($code, $name, $value): ?SettingModel
+    {
+        return $this->updateValue($name, $value, $code);
+    }
+
+    /**
+     * 更新设置值
+     *
+     * @param $name - 设置名称
+     * @param $value - 设置值
+     * @param string $space 设置所属空间
+     * @return SettingModel|null - 返回设置模型
+     * @throws Throwable
+     */
+    private function updateValue($name, $value, string $space): ?SettingModel
+    {
+        if ($value === null) {
+            $value = '';
+        }
+
+        $setting = $this->builder(['space' => $space, 'name' => $name])->first();
+
+        $settingData = [
+            'space' => $space,
+            'name'  => $name,
+            'value' => is_array($value) ? json_encode($value, JSON_UNESCAPED_UNICODE) : $value,
+            'json'  => is_array($value)
+        ];
+
+        if (empty($setting)) {
+            $setting = new SettingModel($settingData);
+            $setting->saveOrFail();
+        } else {
+            $setting->update($settingData);
+        }
+
+        return $setting;
     }
 }
