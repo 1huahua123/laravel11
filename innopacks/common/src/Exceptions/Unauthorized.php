8308d073c8babc2fee2b7ecc215788c65f978792
huarui123 <1850529744@qq.com>
2025-02-09 18:58:53 +0800

    feat: 添加用户认证异常类

---
+++ innopacks/common/src/Exceptions/Unauthorized.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @Author     : Ray
+ * @Date       : 2025/2/9 18:56
+ * @Project    : laravel11
+ * @Description: 用户认证异常类
+ */
+
+namespace InnoShop\Common\Exceptions;
+
+use Exception;
+
+class Unauthorized extends Exception {}
