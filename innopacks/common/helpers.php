b206d11291aab4dccf99677a04a0c8804655f248
Ray <1850529744@qq.com>
2025-02-15 15:24:55 +0800

    feat: 添加系统设置、配置获取及服务提供者增强

--- innopacks/common/helpers.php
+++ innopacks/common/helpers.php
@@ -5,6 +5,40 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use InnoShop\Common\Repositories\SettingRepo;
 
+if (!function_exists('system_setting')) {
+    /**
+     * 获取系统设置值
+     *
+     * 该函数用于获取系统配置中的特定设置值。如果指定的键不存在，则返回默认值。
+     *
+     * @param string     $key     要获取的设置项的键名
+     * @param mixed|null $default 如果指定的键不存在时返回的默认值，默认为 null
+     * @return mixed           返回指定键的设置值，如果键不存在则返回默认值
+     */
+    function system_setting(string $key, mixed $default = null): mixed
+    {
+        // 调用 setting 函数，传入 "system.{$key}" 作为键名，并传入默认值
+        // "system.{$key}" 表示将 "system." 与传入的 $key 拼接成一个完整的键名
+        return setting("system.{$key}", $default);
+    }
+}
+
+if (!function_exists('setting')) {
+    /**
+     * 获取配置项的值
+     *
+     * @param string     $key     配置项的键名
+     * @param mixed|null $default 默认值，如果配置项不存在时返回该值
+     * @return mixed           返回配置项的值，如果配置项不存在则返回默认值
+     */
+    function setting(string $key, mixed $default = null): mixed
+    {
+        // 调用config函数获取配置项的值，配置项的键名为"inno.{$key}"
+        // 如果配置项不存在，则返回$default指定的默认值
+        return config("inno.{$key}", $default);
+    }
+}
+
 if (!function_exists('inno_path')) {
     /**
      * 获取InnoShop路径
--- innopacks/common/src/CommonServiceProvider.php
+++ innopacks/common/src/CommonServiceProvider.php
@@ -2,7 +2,21 @@
 
 namespace InnoShop\Common;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
+use InnoShop\Common\Components\Alert;
+use InnoShop\Common\Components\Forms\Date;
+use InnoShop\Common\Components\Forms\File;
+use InnoShop\Common\Components\Forms\Image;
+use InnoShop\Common\Components\Forms\Images;
+use InnoShop\Common\Components\Forms\Input;
+use InnoShop\Common\Components\Forms\RichText;
+use InnoShop\Common\Components\Forms\SwitchRadio;
+use InnoShop\Common\Components\Forms\Textarea;
+use InnoShop\Common\Components\NoData;
+use InnoShop\Common\Console\PublishFrontTheme;
+use InnoShop\Common\Console\UpdateCountries;
+use InnoShop\Common\Console\UpdateStates;
 
 /**
  * Class CommonServiceProvider
@@ -23,6 +37,11 @@ class CommonServiceProvider extends ServiceProvider
     {
         load_settings();
         $this->registerConfig();
+        $this->registerMigrations();
+        $this->registerCommands();
+        $this->loadMailSettings();
+        $this->loadViewComponents();
+        $this->loadViewTemplates();
     }
 
     /**
@@ -35,11 +54,104 @@ class CommonServiceProvider extends ServiceProvider
         $this->mergeConfigFrom($this->basePath . 'config/innoshop.php', 'innoshop');
     }
 
-    private function registerCommands()
+    /**
+     * 注册数据表迁移文件
+     *
+     * @return void
+     */
+    private function registerMigrations(): void
     {
+        $this->loadMigrationsFrom($this->basePath . 'database/migrations');
+    }
+
+    /**
+     * 定义一个私有方法 registerCommands，用于注册命令
+     *
+     * @return void
+     */
+    private function registerCommands(): void
+    {
+        // 检查应用是否在控制台环境中运行
         if ($this->app->runningInConsole()) {
-            $this->commands();
+            // 如果是在控制台环境中，注册以下命令
+            $this->commands([
+                UpdateCountries::class,
+                UpdateStates::class,
+                PublishFrontTheme::class
+            ]);
         }
+    }
+
+    /**
+     * 用于加载邮件设置
+     *
+     * @return void
+     */
+    private function loadMailSettings(): void
+    {
+        // 获取系统设置中的邮件引擎配置，并将其转换为小写
+        $mailEngine = strtolower(system_setting('email_engine'));
+
+        // 如果邮件引擎配置为空，则直接返回，不执行后续操作
+        if (empty($mailEngine)) {
+            return;
+        }
+
+        // 获取系统设置中的邮件地址，默认为空字符串
+        $storeMailAddress = system_setting('email', '');
+
+        // 设置邮件配置中的默认邮件引擎
+        Config::set('mail.default', $mailEngine);
+        // 设置邮件配置中的发件人地址
+        Config::set('mail.from.address', $storeMailAddress);
+        // 设置邮件配置中的发件人名称，使用应用配置中的应用名称
+        Config::set('mail.from.name', config('app.name'));
+
+        // 如果邮件引擎为 SMTP，则进行 SMTP 相关的配置设置
+        if ($mailEngine == 'smtp') {
+            // 设置 SMTP 邮件发送器的配置信息
+            Config::set('mail.mailers.smtp', [
+                'transport' => 'smtp', // 使用 SMTP 传输方式
+                'host' => system_setting('smtp.host'), // SMTP 服务器地址
+                'port' => system_setting('smtp.port'), // SMTP 服务器端口
+                'encryption' => strtolower(system_setting('smtp.encryption')), // SMTP 加密方式，转换为小写
+                'username' => system_setting('smtp_username'), // SMTP 用户名
+                'password' => system_setting('smtp_password'), // SMTP 密码
+                'timeout' => system_setting('smtp_timeout') // SMTP 超时时间
+            ]);
+        }
+    }
+
+    /**
+     * 用于加载视图组件
+     *
+     * @return void
+     */
+    protected function loadViewComponents(): void
+    {
+        // 调用 loadViewComponentsAs 方法，将一组视图组件以 'common' 前缀注册
+        $this->loadViewComponentsAs('common', [
+            // 注册 'alert' 组件，对应的类是 Alert
+            'alert' => Alert::class,
+            // 注册 'form-input' 组件，对应的类是 Input
+            'form-input' => Input::class,
+            // 注册 'form-date' 组件，对应的类是 Date
+            'form-date' => Date::class,
+            // 注册 'form-image' 组件，对应的类是 Image
+            'form-image' => Image::class,
+            // 注册 'form-file' 组件，对应的类是 File
+            'form-file' => File::class,
+            // 注册 'form-images' 组件，对应的类是 Images
+            'form-images' => Images::class,
+            // 注册 'form-rich-text' 组件，对应的类是 RichText
+            'form-rich-text' => RichText::class,
+            // 注册 'form-switch-radio' 组件，对应的类是 SwitchRadio
+            'form-switch-radio' => SwitchRadio::class,
+            // 注册 'form-textarea' 组件，对应的类是 Textarea
+            'form-textarea' => Textarea::class,
+            // 注册 'no-data' 组件，对应的类是 NoData
+            'no-data' => NoData::class
+        ]);
     }
 
     /**
